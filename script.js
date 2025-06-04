22787be72698bef33bf15718334467c2cab1399c
javlonbek013 <nomonov@gmail.com>
2025-06-04 20:07:47 +0500

    aded humidity

--- script.js
+++ script.js
@@ -7,42 +7,44 @@ async function getWeather() {
     alert("Please enter a city");
     return;
   }
-  
+
   const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${encodeURIComponent(city)}&days=7&aqi=no&alerts=no`;
-  
+
   try {
     const res = await fetch(url);
     const data = await res.json();
-  
+
     if (data.error) {
       document.getElementById("weatherResult").innerHTML = "<p>City not found!</p>";
       return;
     }
-  
+
     let current = data.current;
     let location = data.location;
     let forecast = data.forecast.forecastday;
-  
+
     let html = `
       <h2>${location.name}, ${location.country}</h2>
       <h3>${current.temp_c}°C</h3>
+      <h3>Humidity: ${current.humidity}%</h3>
       <p>${current.condition.text}</p>
       <img src="${current.condition.icon}" alt="${current.condition.text}" />
       <div style="margin-top:20px;"><h3>7-Day Forecast</h3></div>
       <div class="forecast">
     `;
-  
+
     forecast.forEach(day => {
       html += `
         <div class="day">
           <p>${day.date}</p>
           <img src="${day.day.condition.icon}" alt="${day.day.condition.text}" />
           <p>${day.day.avgtemp_c}°C</p>
+      <h3>Humidity: ${current.humidity}%</h3>
           <p>${day.day.condition.text}</p>
         </div>
       `;
     });
-  
+
     html += `</div>`;
     document.getElementById("weatherResult").innerHTML = html;
   } catch (error) {
@@ -51,7 +53,6 @@ async function getWeather() {
   }
 }
 
-// Toggle input visibility and search on icon click
 const searchIcon = document.getElementById("searchIcon");
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
